c606048824c51eecfb24689874d10bcbda122aca
epikziom39 <105452781+epikziom39@users.noreply.github.com>
2022-05-15 18:07:43 +0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -1,6 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here https://dashboard.ngrok.com/get-started/your-authtoken (Copy and Ctrl+V to paste then press Enter): " CRP
+read -p "Paste ngrok authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP
 echo region
 read -p "region: " dick
