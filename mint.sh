18315b3f86f5f369e03502f3b10e9ae881aa46b0
epikziom39 <105452781+epikziom39@users.noreply.github.com>
2022-05-15 13:12:23 +0400

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -2,7 +2,7 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/nu
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here https://dashboard.ngrok.com/get-started/your-authtoken (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP
-echo region us, eu, au, ap, sa, jp,
+echo region
 read -p "region: " region
 nohup ./ngrok tcp -region $region 5900 &>/dev/null &
 mkdir mint
@@ -17,4 +17,4 @@ echo making disk
 qemu-img create mint.vdi 100G
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-sudo qemu-system-x86_64 -cdrom linuxmint-20.3-cinnamon-64bit.iso -vnc :0 -hda win10.vdi -smp cores=8  -m 12000M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -cdrom linuxmint-20.3-cinnamon-64bit.iso -vnc :0 -hda mint.vdi -smp cores=8  -m 12000M -machine usb=on -device usb-tablet > /dev/null 2>&1
